69daad0c7d0b1c322583b68c014680f08f518b4e
Evan <evanmohammedsaleh@gmail.com>
2020-12-11 15:46:37 -0600

    added description to my projects

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -17,18 +17,19 @@ export default function Projects() {
                                     <img className='projects--image' src={VibeApp} alt="Vibe" ></img>
                                 
                                 <div className="projects--content">
-                                <span class="highlight">React JS / MongoDB/ CSS/ Bootstrap</span>
+                                <span class="highlight">Using ReactJs, Unsplash's powerful image API, JWT authentication, and MaterializeCss to empower the user to curate their own collection of personal collection of photography from some of the world's most talented photographers.</span>
                                 </div>
+
                                 <a className="projects--btn" href="https://github.com/dchoi20/Vibe">
                                      Learn More
                                 </a>
                             </div>
                             <div className="projects--card">
                                 
-                                    <img className='projects--image' src={MoneyTrend} alt="Vibe" ></img>
+                                    <img className='projects--image' src={MoneyTrend} alt="Money Trend" ></img>
                                 
                                 <div className="projects--content">
-                                <span class="highlight">JavaScript / MySQL /Scss</span>
+                                <span class="highlight">MoneyTrend allows a user to create an account as a unique user. They can then add in their incomes, with dollar amounts and details. The expenses page allows the user to create a table of their budget, categorized for their convenience.</span>
                                 </div>
                                 <a className="projects--btn" href="https://github.com/EvanMohammed/MoneyTrend">
                                     Learn More
@@ -44,10 +45,10 @@ export default function Projects() {
                         <div className='projects--group'>
                             <div className="projects--card">
                                 
-                                    <img className='projects--image' id='projects--readme' src={ReadMe} alt="Vibe" ></img>
+                                    <img className='projects--image' id='projects--readme' src={ReadMe} alt="ReadMe" ></img>
                                 
                                 <div className="projects--content" >
-                                <span class="highlight">NodeJs / Inquirer</span>
+                                <span class="highlight">Using NodeJs and Inquirer, I managed to create A simple Node App for future Developers so they can create their .MD files for their future projects in a much easier way than writing it from scratch</span>
                                 </div>
                                 <a className="projects--btn" href="https://github.com/EvanMohammed/README.md-Generator">
                                     Learn More
@@ -58,7 +59,7 @@ export default function Projects() {
                                     <img className='projects--image' src={Fitness} alt="Vibe" ></img>
                                 
                                 <div className="projects--content">
-                                <span class="highlight">MongoDB / ChartJs /API</span>
+                                <span class="highlight"> An App to manage your Personal Workout and collecting data as well by using the following libraries,  HTML / CSS / JavaScript/ MongoDB.</span>
                                 </div>
                                 <a className="projects--btn" href="https://github.com/EvanMohammed/Workout-Tracker">
                                     Learn More
